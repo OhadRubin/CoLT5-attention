cde0f08c69e0cb86d49efb165efd5d5ae3e01516
OhadRubin <iohadrubin@gmail.com>
2024-01-18 11:03:17 +0200

    ..

--- colt5_attention/transformer_block.py
+++ colt5_attention/transformer_block.py
@@ -638,8 +638,8 @@ class ConditionalRoutedAttention(nn.Module):
         num_heavy_tokens_kv = default(num_heavy_tokens_kv, self.num_heavy_tokens_kv)
 
         # light local attention sees all tokens in a limited context
-        with torch.cuda.amp.autocast(enabled=False):
-            x = x.to(torch.bfloat16)
+            # x = x.to()
+        with torch.cuda.amp.autocast(enabled=True,dtype=torch.bfloat16):
             light_out = self.light_attn(x, mask = mask)
 
         # route tokens appropriately for heavy branch
